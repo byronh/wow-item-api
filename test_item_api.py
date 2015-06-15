9065421459a1534572c252052b546b6b78a91255
Byron Henze <byronh@gmail.com>
2015-06-15 09:11:52 -0700

    Remove extra setup method, reorganize

--- test_item_api.py
+++ test_item_api.py
@@ -14,19 +14,6 @@ ITEM_API_ENDPOINT = 'http://{region}/api/wow/item/{object_id}'
 ITEM_SET_API_ENDPOINT = 'http://{region}/api/wow/item/set/{object_id}'
 
 
-def pytest_generate_tests(metafunc):
-    """ Generate additional test cases for test functions with both the `region` and `locale` parameters present in
-    order to allow testing of all possible regions and locale combinations. This allows any failures that occur for
-    a particular region/locale combination to be reported as a separate test failure. """
-    if 'region' in metafunc.fixturenames and 'locale' in metafunc.fixturenames:
-        with open('regions.json', 'r') as regions_file:
-            regions_data = json.load(regions_file)
-        region_locale_combinations = []
-        for region_name, locales in regions_data.items():
-            region_locale_combinations.extend([region_name, locale] for locale in locales)
-        metafunc.parametrize('region,locale', region_locale_combinations)
-
-
 class TestItemAPI(object):
 
     def test_individual_item(self):
@@ -41,9 +28,13 @@ class TestItemAPI(object):
 
     @pytest.mark.parametrize('invalid_item_id', ['', -1, 'magical crawdad'])
     def test_invalid_parameter(self, invalid_item_id):
-        """ Ensure that requests using an invalid item ID return a 404 error. """
+        """ Ensure that requests using an invalid item ID or item set ID return a 404 error. """
         with pytest.raises(HTTPError) as e:
             self.call_api(ITEM_API_ENDPOINT, object_id=invalid_item_id)
+        self.assert_failure(e.value.response, error_code=404)
+
+        with pytest.raises(HTTPError) as e:
+            self.call_api(ITEM_SET_API_ENDPOINT, object_id=invalid_item_id)
         self.assert_failure(e.value.response, error_code=404)
 
     def test_invalid_endpoint(self):
@@ -73,8 +64,6 @@ class TestItemAPI(object):
 
         assert 'content-language' in response.headers, 'response headers missing content-language'
         response_locale = response.headers['content-language'].replace('-', '_', 1)
-        assert response_locale in self.regions_data[region], \
-            'response not using a supported locale: {}'.format(response_locale)
         assert response_locale == locale, \
             'response locale {} does not match requested locale {}'.format(response_locale, locale)
 
@@ -160,12 +149,6 @@ class TestItemAPI(object):
         assert data['reason'] == 'Invalid Application'
 
     """ Internal helper methods. """
-    @classmethod
-    def setup_class(cls):
-        """ Loads the JSON file containing region and locale data prior to the test run. """
-        with open('regions.json', 'r') as regions_file:
-            cls.regions_data = json.load(regions_file)
-
     def call_api(self, endpoint, object_id, region=DEFAULT_REGION, **params):
         """ Calls a RESTful API endpoint. Will raise an HTTPError if a 4xx or 5xx error code is encountered. """
         response = requests.get(endpoint.format(region=region, object_id=object_id), params)
@@ -181,6 +164,19 @@ class TestItemAPI(object):
         assert response.status_code == error_code
         data = response.json()
         assert data['status'] == 'nok'
+
+
+def pytest_generate_tests(metafunc):
+    """ Generate additional test cases for test functions with both the `region` and `locale` parameters present in
+    order to allow testing of all possible regions and locale combinations. This allows any failures that occur for
+    a particular region/locale combination to be reported as a separate test failure. """
+    if 'region' in metafunc.fixturenames and 'locale' in metafunc.fixturenames:
+        with open('regions.json', 'r') as regions_file:
+            regions_data = json.load(regions_file)
+        region_locale_combinations = []
+        for region_name, locales in regions_data.items():
+            region_locale_combinations.extend([region_name, locale] for locale in locales)
+        metafunc.parametrize('region,locale', region_locale_combinations)
 
 
 if __name__ == '__main__':
